7de7a08e78f148a07bad04e2e82d99756b5c18d8
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2026-02-21 14:11:27 +0100

    generateShortBread function

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UrlsModule } from './urls/urls/urls.module';
 @Module({
-  imports: [],
+  imports: [UrlsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/utils/short-code.ts
@@ -0,0 +1,12 @@
+// this function should take in no arguments but should generate a randomshort code when called, this code is what will be the shortbread for the long address.
+export function generateShortBread() {
+  const BASE62_ALPHABET =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const shortCodeLength = 6;
+  let shortCode = '';
+  for (let i = 0; i < shortCodeLength; i++) {
+    shortCode +=
+      BASE62_ALPHABET[Math.floor(Math.random() * BASE62_ALPHABET.length)];
+  }
+  return shortCode;
+}

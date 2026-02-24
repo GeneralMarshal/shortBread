b8d084821708c108292b43737214fd8e56637c29
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2026-02-24 15:37:48 +0100

    throttler module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,28 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UrlsModule } from './urls/urls.module';
 import { RedisModule } from './redis/redis.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UrlsModule, RedisModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60 * 1000,
+          limit: 30,
+        },
+      ],
+    }),
+    ConfigModule.forRoot(),
+    UrlsModule,
+    RedisModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+    AppService,
+    PrismaService,
+  ],
 })
 export class AppModule {}
